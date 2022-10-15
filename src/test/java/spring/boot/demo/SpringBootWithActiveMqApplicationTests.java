54da752b2647db136108f9dd21d6b53f2d898077
duongCute123 <89206817+duongCute123@users.noreply.github.com>
2022-10-15 15:21:35 +0700

    add project

---
+++ src/test/java/spring/boot/demo/SpringBootWithActiveMqApplicationTests.java
@@ -0,0 +1,13 @@
+package spring.boot.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootWithActiveMqApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
