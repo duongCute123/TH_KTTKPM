c125f429832924ee3eceaebf1f6c597b67d484a0
duongCute123 <89206817+duongCute123@users.noreply.github.com>
2022-10-15 22:44:01 +0700

    finish spring boot with ActiveMQ

--- src/main/java/spring/boot/demo/SpringBootWithActiveMqApplication.java
+++ src/main/java/spring/boot/demo/SpringBootWithActiveMqApplication.java
@@ -2,8 +2,10 @@ package spring.boot.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
+@ComponentScan(basePackages = "spring.boot.demo")
 public class SpringBootWithActiveMqApplication {
 
 	public static void main(String[] args) {
